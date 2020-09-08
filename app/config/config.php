44d8615b3d8bece60c83da9d708dcca5c5613d5d
c5h11oh <c5h11oh@P34G-Ubuntu18.04>
2020-09-08 19:19:46 +0800

    minor fix, ready for develop in native Ubuntu, not Docker

--- app/config/config.php
+++ app/config/config.php
@@ -1,19 +1,19 @@
 <?php
   //DB params
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', '123456');
-  define('DB_NAME', 'shareposts');
+  define('DB_HOST', 'mysql');
+  define('DB_USER', 'svob');
+  define('DB_PASS', 'oi4OMUk9Xw02');
+  define('DB_NAME', 'svob');
 
 
   //Define APPROOT
   define('APPROOT', dirname(dirname(__FILE__)));
   
   //Define URLROOT
-  define('URLROOT', 'http://localhost/shareposts');
+  define('URLROOT', 'http://localhost');
 
   //Define SITENAME
-  define('SITENAME', 'The SharePosts');
+  define('SITENAME', 'Hello');
 
   //App Version
   define('APPVERSION', '1.0.0');
